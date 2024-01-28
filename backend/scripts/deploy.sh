600e172743f745bfaed5843f700757c75d383785
Kim Jiyoon <rlawldbs0911@naver.com>
2024-01-28 23:27:03 +0900

    #7 hotfix: deploy.sh 오타 수정

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -6,11 +6,10 @@ CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 

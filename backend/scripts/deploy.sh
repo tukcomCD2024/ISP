f99ab03596b0cbd772a8605d4382ab22bff8a263
Kim Jiyoon <rlawldbs0911@naver.com>
2024-01-29 00:57:41 +0900

    fix: deploy.sh 수정

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,28 +1,19 @@
 REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-./gradlew build
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/build/libs/backend-0.0.1-SNAPSHOT.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-nohup java -jar $JAR_NAME > $REPOSITORY/build/libs/nohup.out 2>&1 &
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &

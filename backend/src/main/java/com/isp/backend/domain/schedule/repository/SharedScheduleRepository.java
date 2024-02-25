8128bb29e9c55287024f1b9fa4b3774a0220bdf1
Dayeon-Hong <dayon0325@gmail.com>
2024-02-25 15:50:06 +0900

    feat: 스케줄관련 CRUD + S3연결  + 글로벌에러설정

--- backend/src/main/java/com/isp/backend/domain/schedule/repository/SharedScheduleRepository.java
+++ backend/src/main/java/com/isp/backend/domain/schedule/repository/SharedScheduleRepository.java
@@ -3,6 +3,6 @@ package com.isp.backend.domain.schedule.repository;
 import com.isp.backend.domain.schedule.entity.SharedSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SharedScheduleRepository extends JpaRepository<SharedSchedule, String> {
+public interface SharedScheduleRepository extends JpaRepository<SharedSchedule, Long> {
 
 }
--- backend/src/main/java/com/isp/backend/global/exception/member/MemberNotFoundException.java
+++ backend/src/main/java/com/isp/backend/global/exception/member/MemberNotFoundException.java
@@ -8,5 +8,6 @@ public class MemberNotFoundException extends CustomException {
     public MemberNotFoundException() {
         super(ErrorCode.MEMBER_NOT_FOUND);
     }
+
 }
 

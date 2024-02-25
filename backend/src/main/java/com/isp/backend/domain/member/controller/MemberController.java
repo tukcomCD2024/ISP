e355c104bef97cc1e291a16d0be92d0809cc750c
Dayeon-Hong <dayon0325@gmail.com>
2024-02-26 00:36:46 +0900

    hotfix : member API url 수정

--- backend/src/main/java/com/isp/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/isp/backend/domain/member/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
 

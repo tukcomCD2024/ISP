5d04b2aa3d6ffa4cea6ad75754d0a0056e9f2b47
Dayeon-Hong <dayon0325@gmail.com>
2024-02-25 15:50:06 +0900

    feat: 스케줄관련 CRUD + S3연결  + 글로벌에러설정

--- backend/src/main/java/com/isp/backend/global/exception/ErrorCode.java
+++ backend/src/main/java/com/isp/backend/global/exception/ErrorCode.java
@@ -15,7 +15,10 @@ public enum ErrorCode {
     MEMBER_NOT_ACTIVATED(HttpStatus.BAD_REQUEST, "U003", "사용자가 활성화 상태가 아닙니다."),
     ACCESS_TOKEN_IS_INVALID(HttpStatus.UNAUTHORIZED, "U004","엑세스 토큰이 유효하지 않습니다."),
     REFRESH_TOKEN_IS_INVALID(HttpStatus.UNAUTHORIZED, "U005", "리프레시 토큰이 유효하지 않습니다."),
-    COUNTRY_NOT_FOUND(HttpStatus.BAD_REQUEST, "S001","여행할 국가를 찾을 수 없습니다.");
+    COUNTRY_NOT_FOUND(HttpStatus.NOT_FOUND, "S001","여행할 국가를 찾을 수 없습니다."),
+    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "S002","여행 일정을 찾을 수 없습니다."),
+    NOT_YOUR_SCHEDULE(HttpStatus.UNAUTHORIZED, "S003" , "사용자의 여행 일정이 아닙니다."),
+    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "S004","이미지를 찾을 수 없습니다.");
 
 
     private HttpStatus status;
---
+++ backend/src/main/java/com/isp/backend/global/s3/BucketDir.java
@@ -0,0 +1,16 @@
+package com.isp.backend.global.s3;
+
+import lombok.Getter;
+
+@Getter
+public enum BucketDir {
+    IMAGE("HowAboutTrip-Image"),         // 상품 사진
+    PHOTO("HowAboutTrip-Photo"), ;        // 여행 중 포토
+
+    private String dirName;
+
+    BucketDir(String dirName) {
+        this.dirName = dirName;
+    }
+
+}

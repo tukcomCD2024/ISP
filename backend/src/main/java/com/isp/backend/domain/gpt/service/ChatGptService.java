af73e42d70ce9fd5573be76014b9d7a4cea80e81
Kim Jiyoon <rlawldbs0911@naver.com>
2024-02-29 01:17:25 +0900

    feat: gpt 요청 및 응답을 위한 컨트롤러와 서비스 생성

---
+++ backend/src/main/java/com/isp/backend/domain/gpt/service/ChatGptService.java
@@ -0,0 +1,71 @@
+package com.isp.backend.domain.gpt.service;
+
+import com.isp.backend.domain.gpt.config.ChatGptConfig;
+import com.isp.backend.domain.gpt.dto.ChatGptRequestDTO;
+import com.isp.backend.domain.gpt.dto.ChatGptResponseDTO;
+import com.isp.backend.domain.gpt.dto.QuestionRequestDTO;
+import com.isp.backend.domain.gpt.entity.ChatGptMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatGptService {
+    private final RestTemplate restTemplate;
+
+    @Value("${api-key.chat-gpt}")
+    private String apiKey;
+
+    public HttpEntity<ChatGptRequestDTO> buildHttpEntity(ChatGptRequestDTO chatGptRequestDTO) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.parseMediaType(ChatGptConfig.MEDIA_TYPE));
+        httpHeaders.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + apiKey);
+        return new HttpEntity<>(chatGptRequestDTO, httpHeaders);
+    }
+
+    public ChatGptResponseDTO getResponse(HttpEntity<ChatGptRequestDTO> chatGptRequestHttpEntity){
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(60000);
+        requestFactory.setReadTimeout(60 * 1000);
+        restTemplate.setRequestFactory(requestFactory);
+
+        ResponseEntity<ChatGptResponseDTO> responseEntity = restTemplate.postForEntity(
+                ChatGptConfig.CHAT_URL,
+                chatGptRequestHttpEntity,
+                ChatGptResponseDTO.class);
+
+        return responseEntity.getBody();
+    }
+    public ChatGptResponseDTO askQuestion(QuestionRequestDTO questionRequestDTO){
+        List<ChatGptMessage> messages = new ArrayList<>();
+        messages.add(ChatGptMessage.builder()
+                .role(ChatGptConfig.ROLE)
+                .content(questionRequestDTO.getQuestion())
+                .build());
+        System.out.println(messages);
+        return this.getResponse(
+                this.buildHttpEntity(
+                        new ChatGptRequestDTO(
+                                ChatGptConfig.CHAT_MODEL,
+                                ChatGptConfig.MAX_TOKEN,
+                                ChatGptConfig.TEMPERATURE,
+                                ChatGptConfig.STREAM,
+                                messages
+                        )
+                )
+        );
+    }
+}

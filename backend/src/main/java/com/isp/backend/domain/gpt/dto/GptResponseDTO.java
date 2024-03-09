0d95d038b45e4952c525cd6eb77e95ac103d0f07
Kim Jiyoon <rlawldbs0911@naver.com>
2024-03-10 01:42:48 +0900

    fix: 불필요한 응답 멤버변수 삭제

--- backend/src/main/java/com/isp/backend/domain/gpt/dto/GptResponseDTO.java
+++ backend/src/main/java/com/isp/backend/domain/gpt/dto/GptResponseDTO.java
@@ -1,6 +1,5 @@
 package com.isp.backend.domain.gpt.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.isp.backend.domain.gpt.entity.GptMessage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,30 +12,11 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GptResponseDTO {
-    private String id;
-    private String object;
-    private long created;
-    private String model;
-    private Usage usage;
     private List<Choice> choices;
-
-    @Getter
-    @Setter
-    public static class Usage {
-        @JsonProperty("prompt_tokens")
-        private int promptTokens;
-        @JsonProperty("completion_tokens")
-        private int completionTokens;
-        @JsonProperty("total_tokens")
-        private int totalTokens;
-    }
 
     @Getter
     @Setter
     public static class Choice {
         private GptMessage message;
-        @JsonProperty("finish_reason")
-        private String finishReason;
-        private int index;
     }
 }

80ac0118770ef5d7257ec58a50a29ac00923f25f
Kim Jiyoon <rlawldbs0911@naver.com>
2024-03-09 23:33:39 +0900

    fix: Gpt 응답 지연에 따른 최대 토큰 수정

--- backend/src/main/java/com/isp/backend/domain/gpt/config/GptConfig.java
+++ backend/src/main/java/com/isp/backend/domain/gpt/config/GptConfig.java
@@ -7,7 +7,7 @@ public class GptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
     public static final String CHAT_MODEL = "gpt-3.5-turbo";
-    public static final Integer MAX_TOKEN = 300;
+    public static final Integer MAX_TOKEN = 3000;
     public static final Boolean STREAM = false;
     public static final String ROLE = "user";
     public static final Double TEMPERATURE = 0.6;

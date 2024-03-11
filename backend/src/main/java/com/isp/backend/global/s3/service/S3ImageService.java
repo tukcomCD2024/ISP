d8dc33dd55b70b77e5c0fdaf5efe3cd00480fb47
Kim Jiyoon <rlawldbs0911@naver.com>
2024-03-11 23:21:42 +0900

    feat: S3 컨트롤러로부터 들어온 요청을 처리할 서비스 구현

---
+++ backend/src/main/java/com/isp/backend/global/s3/service/S3ImageService.java
@@ -0,0 +1,19 @@
+package com.isp.backend.global.s3.service;
+
+import com.isp.backend.global.s3.repository.S3ImageRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class S3ImageService {
+    S3ImageRepository s3ImageRepository;
+
+    public String create(MultipartFile file) {
+        return s3ImageRepository.save(file);
+    }
+}

ec840ac29172694b52820aaab6b9615771578ed4
Kim Jiyoon <rlawldbs0911@naver.com>
2024-03-17 00:42:59 +0900

    hotfix: request mapping 명수를 복수형으로 수정

--- backend/src/main/java/com/isp/backend/domain/country/controller/CountryController.java
+++ backend/src/main/java/com/isp/backend/domain/country/controller/CountryController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/country")
+@RequestMapping("/countries")
 @RequiredArgsConstructor
 public class CountryController {
 

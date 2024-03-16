407196fcb17bf1031a5c6485e3b79cd4ae6d9f93
Dayeon-Hong <dayon0325@gmail.com>
2024-03-17 00:04:17 +0900

    hotfix : api url 주소 country 수정

--- backend/src/main/java/com/isp/backend/domain/country/controller/CountryController.java
+++ backend/src/main/java/com/isp/backend/domain/country/controller/CountryController.java
@@ -2,13 +2,15 @@ package com.isp.backend.domain.country.controller;
 
 import com.isp.backend.domain.country.dto.LocationResponseDTO;
 import com.isp.backend.domain.country.service.CountryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/country")
+@RequiredArgsConstructor
 public class CountryController {
 
     @Autowired

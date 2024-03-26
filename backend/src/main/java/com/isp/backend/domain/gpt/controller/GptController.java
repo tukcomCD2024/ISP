30780088c2d86db7e8726e8906e4f858194b6db8
Kim Jiyoon <rlawldbs0911@naver.com>
2024-03-26 14:54:20 +0900

    fix: 불필요한 ResponseEntity 삭제

--- backend/src/main/java/com/isp/backend/domain/gpt/controller/GptController.java
+++ backend/src/main/java/com/isp/backend/domain/gpt/controller/GptController.java
@@ -4,21 +4,18 @@ import com.isp.backend.domain.gpt.dto.request.GptScheduleRequest;
 import com.isp.backend.domain.gpt.dto.response.GptScheduleResponse;
 import com.isp.backend.domain.gpt.service.GptService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/gpt")
 @RestController
+@ResponseStatus(value = HttpStatus.CREATED)
 public class GptController {
     private final GptService gptService;
 
     @PostMapping("/schedules")
-    public ResponseEntity<GptScheduleResponse> sendQuestion(@RequestBody GptScheduleRequest gptScheduleRequest) {
-        GptScheduleResponse gptScheduleResponse = gptService.askQuestion(gptScheduleRequest);
-        return ResponseEntity.ok(gptScheduleResponse);
+    public GptScheduleResponse sendQuestion(@RequestBody GptScheduleRequest gptScheduleRequest) {
+        return gptService.askQuestion(gptScheduleRequest);
     }
 }

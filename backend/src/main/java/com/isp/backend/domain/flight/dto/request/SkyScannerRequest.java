f413bf57a5abbc819367ad666ffd4b5c36835114
Dayeon-Hong <dayon0325@gmail.com>
2024-05-12 18:39:33 +0900

    feat : 스카이스캐너 url 생성 DTO 수정

---
+++ backend/src/main/java/com/isp/backend/domain/flight/dto/request/SkyScannerRequest.java
@@ -0,0 +1,28 @@
+package com.isp.backend.domain.flight.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class SkyScannerRequest {
+
+    private String departureIataCode;
+
+    private String arrivalIataCode;
+
+    private String departureDate;
+
+    private String returnDate;
+
+    private int adults;
+
+    private int children;
+
+    private String departureTime;
+
+    private int transferCount;
+
+}

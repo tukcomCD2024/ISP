83caebc60a241973642e63ba8da2da9f4cb46fd7
Dayeon-Hong <dayon0325@gmail.com>
2024-05-12 18:40:07 +0900

    feat : flight 관련 DB 데이터, DTO 정리

--- backend/src/main/java/com/isp/backend/domain/country/repository/CountryRepository.java
+++ backend/src/main/java/com/isp/backend/domain/country/repository/CountryRepository.java
@@ -8,8 +8,8 @@ import java.util.Optional;
 public interface CountryRepository extends JpaRepository<Country, Long> {
 
     Optional<Country> findIdByCity(String city); // 여행나라 이름으로 id 찾기
-
-    Country findCountryById(Long countryId); //
+    Country findCountryById(Long countryId);
     Optional<Country> findAirportCodeByCity(String countryName); // 나라 이름으로 공항 코드 찾기
+    Optional<Country> findCountryByAirportCode(String airportCode);
 
 }

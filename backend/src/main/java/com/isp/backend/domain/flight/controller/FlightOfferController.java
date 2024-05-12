dd1a8c8092edc608165deed0e86dea14074fb075
Dayeon-Hong <dayon0325@gmail.com>
2024-05-12 18:37:09 +0900

    feat : 스카이스캐너 url 생성, 좋아요 관련 API

--- backend/src/main/java/com/isp/backend/domain/flight/controller/FlightOfferController.java
+++ backend/src/main/java/com/isp/backend/domain/flight/controller/FlightOfferController.java
@@ -1,8 +1,13 @@
 package com.isp.backend.domain.flight.controller;
 
 import com.amadeus.exceptions.ResponseException;
-import com.isp.backend.domain.flight.service.FlightOfferService;
+import com.isp.backend.domain.flight.dto.request.FlightLikeRequest;
+import com.isp.backend.domain.flight.dto.request.SkyScannerRequest;
+import com.isp.backend.domain.flight.dto.response.FlightLikeResponse;
+import com.isp.backend.domain.flight.service.FlightOfferServiceImpl;
 import com.isp.backend.domain.flight.dto.request.FlightSearchRequest;
+import com.isp.backend.global.exception.flight.FlightSearchFailedException;
+import com.isp.backend.global.exception.flight.SkyScannerGenerateFailedException;
 import com.isp.backend.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bookings/flights")
@@ -20,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FlightOfferController {
 
     @Autowired
-    private FlightOfferService flightOfferService;
+    private FlightOfferServiceImpl flightOfferServiceImpl;
 
     /** 항공권 검색 API **/
     @GetMapping("/search")
@@ -28,7 +32,7 @@ public class FlightOfferController {
                                                   @RequestBody FlightSearchRequest request) {
         String memberUid = customUserDetails.getUsername();
         try {
-            String flightOffersJson = flightOfferService.getFlightOffers(request);
+            String flightOffersJson = flightOfferServiceImpl.getFlightOffers(request);
             return ResponseEntity.ok(flightOffersJson);
         } catch (ResponseException e) {
             throw new FlightSearchFailedException();
@@ -36,13 +40,44 @@ public class FlightOfferController {
     }
 
 
-    /** 항공권 다른 사이트로 연결 API **/
+    /** 항공권 선택시 스카이스캐너 사이트로 연결 API **/
+    @PostMapping("/skyscanner")
+    public ResponseEntity<String> getFlightSearchUrl(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                     @RequestBody SkyScannerRequest request) {
+        String memberUid = customUserDetails.getUsername();
+        try {
+            String skyscannerUrl = flightOfferServiceImpl.generateSkyscannerUrl(request);
+            return ResponseEntity.ok("{\"skyscannerUrl\": \"" + skyscannerUrl + "\"}");
+        } catch (Exception e) {
+            throw new SkyScannerGenerateFailedException();
+        }
+    }
 
 
-    /** 항공권 좋아요 API **/
+    /** 항공권 좋아요 저장 API **/
+    @PostMapping("/like")
+    public ResponseEntity<Void> addLikeFlight(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                              @RequestBody FlightLikeRequest flightLikeRequest) {
+        String memberUid = customUserDetails.getUsername();
+        flightOfferServiceImpl.addLikeFlight(memberUid, flightLikeRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
+    /** 항공권 나의 좋아요 목록 불러오기 API **/
+    @GetMapping("/likes")
+    public ResponseEntity<List<FlightLikeResponse>> getLikedFlights(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        String memberUid = customUserDetails.getUsername();
+        List<FlightLikeResponse> likedFlights = flightOfferServiceImpl.getLikedFlights(memberUid);
+        return ResponseEntity.ok(likedFlights);
+    }
 
-    /** 항공권 좋아요 목록 불러오기 API **/
-
+    /** 항공권 나의 좋아요 삭제 API **/
+    @DeleteMapping("/like/{id}")
+    public ResponseEntity<Void> deleteLikeFlight(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                 @PathVariable Long id) {
+        String memberUid = customUserDetails.getUsername();
+        flightOfferServiceImpl.deleteLikeFlight(memberUid, id);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- backend/src/main/java/com/isp/backend/domain/flight/service/FlightOfferService.java
+++ backend/src/main/java/com/isp/backend/domain/flight/service/FlightOfferService.java
@@ -1,59 +1,27 @@
 package com.isp.backend.domain.flight.service;
 
-import com.amadeus.Amadeus;
-import com.amadeus.Params;
 import com.amadeus.exceptions.ResponseException;
-import com.amadeus.resources.FlightOfferSearch;
-import com.google.gson.Gson;
 import com.isp.backend.domain.country.entity.Country;
-import com.isp.backend.domain.country.repository.CountryRepository;
+import com.isp.backend.domain.flight.dto.request.FlightLikeRequest;
 import com.isp.backend.domain.flight.dto.request.FlightSearchRequest;
-import com.isp.backend.domain.flight.mapper.FlightOfferProcessor;
+import com.isp.backend.domain.flight.dto.request.SkyScannerRequest;
+import com.isp.backend.domain.flight.dto.response.FlightLikeResponse;
 import com.isp.backend.global.exception.schedule.CountryNotFoundException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-@Service
-public class FlightOfferService {
+import java.util.List;
 
-    private final Amadeus amadeus;
-    private final CountryRepository countryRepository;
-    private final FlightOfferProcessor flightOfferProcessor ;
+public interface FlightOfferService {
 
+    String getFlightOffers(FlightSearchRequest request) throws ResponseException;
 
-    /** 항공편 조회 **/
-    public String getFlightOffers(FlightSearchRequest request) throws ResponseException {
+    String generateSkyscannerUrl(SkyScannerRequest request);
 
-        String originLocationCode = findAirportCode(request.getOriginCity());
-        String destinationLocationCode = findAirportCode(request.getDestinationCity());
+    @Transactional
+    void addLikeFlight(String uid, FlightLikeRequest flightLikeRequest);
 
-        FlightOfferSearch[] flightOffers = amadeus.shopping.flightOffersSearch.get(
-                Params.with("originLocationCode", originLocationCode)
-                        .and("destinationLocationCode", destinationLocationCode)
-                        .and("departureDate", request.getDepartureDate())
-                        .and("returnDate", request.getReturnDate())
-                        .and("adults", request.getAdults())
-                        .and("children", request.getChildren())
-                        .and("max", request.getMax())
-                        .and("nonStop", request.isNonStop())
-                        .and("currencyCode","KRW")  // 원화 설정 -> 추후 유저에게 입력받을 수 있게 변경
-        );
+    List<FlightLikeResponse> getLikedFlights(String memberUid);
 
-        Gson gson = new Gson();
-        String flightOffersJson = gson.toJson(flightOffers);
-        return flightOfferProcessor.processFlightOffers(flightOffersJson);
-    }
-
-
-    /** 공항 코드 찾기 **/
-    private String findAirportCode(String countryName) {
-        Country findCountry = countryRepository.findAirportCodeByCity(countryName)
-                .orElseThrow(()-> new CountryNotFoundException());
-        return findCountry.getAirportCode();
-    }
-
-
+    @Transactional
+    void deleteLikeFlight(String memberUid, Long id);
 }

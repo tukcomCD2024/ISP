cbb8911ec630f40be838f5068078ead66951d42d
Kim Jiyoon <rlawldbs0911@naver.com>
2024-05-13 01:21:37 +0900

    feat: #61 gpt-4-turbo api 도입

--- backend/src/main/java/com/isp/backend/domain/gpt/config/GptConfig.java
+++ backend/src/main/java/com/isp/backend/domain/gpt/config/GptConfig.java
@@ -6,20 +6,22 @@ import org.springframework.context.annotation.Configuration;
 public class GptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String CHAT_MODEL = "gpt-3.5-turbo-16k";
-    public static final Integer MAX_TOKEN = 3000;
+    public static final String CHAT_MODEL = "gpt-4-turbo";
+    public static final Integer MAX_TOKEN = 4095;
     public static final Boolean STREAM = false;
     public static final String ROLE = "user";
-    public static final Double TEMPERATURE = 0.6;
+    public static final Double TEMPERATURE = 1.5;
     public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
     public static final String CHAT_URL = "https://api.openai.com/v1/chat/completions";
     public static final String PROMPT = """
+                        You're a great travel agency staff member.
                         I would like you to plan a package tour program to plan your travel itinerary.
                         
                         Also, plan your itinerary based on the travel distance and famous tourist destinations.
-                        If possible, please also provide the cost.
                         Also, be sure to include the name of the landmark or location.
+                        I want to visit a maximum of three attractions per day.
                         Make sure to plan for at least 10 hours of activity each day.
+                        Please write the itinerary without using any special characters, only in plain text.
                         
                         I would like the format to be as follows:
                         For example
@@ -60,5 +62,8 @@ public class GptConfig {
                         한국에 귀국하는 날짜: %s
                         
                         No need to say anything else, just plan your schedule right away.
-                        Please create the result in Korean.""";
+                        Please create the result in Korean.
+                        And only include schedule-related content in the schedule you're creating.
+                        Do not add any information I haven't provided to you.
+                        Under no circumstances should you include any activities other than traveling. Absolutely not.""";
 }

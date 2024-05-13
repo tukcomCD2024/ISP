003bf73ed5999a5e1804f30ee98e307e66823193
Dayeon-Hong <dayon0325@gmail.com>
2024-05-13 18:59:16 +0900

    fix : Error 코드 공통으로 쓸수 있도록 변수명 변경

--- backend/src/main/java/com/isp/backend/global/exception/flight/AmadeusSearchFailedException.java
+++ backend/src/main/java/com/isp/backend/global/exception/flight/AmadeusSearchFailedException.java
@@ -3,10 +3,10 @@ package com.isp.backend.global.exception.flight;
 import com.isp.backend.global.exception.CustomException;
 import com.isp.backend.global.exception.ErrorCode;
 
-public class FlightSearchFailedException extends CustomException {
+public class AmadeusSearchFailedException extends CustomException {
 
-    public FlightSearchFailedException() {
-        super(ErrorCode.FLIGHT_SEARCH_FAILED);
+    public AmadeusSearchFailedException() {
+        super(ErrorCode.AMADEUS_SEARCH_FAILED);
     }
 
 }

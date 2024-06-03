20092b24ac7e9dee1b9084e5aaa39645902a24d9
Dayeon-Hong <dayon0325@gmail.com>
2024-06-03 18:26:41 +0900

    feat : 스케줄 별 체크리스트 API 추가

--- backend/src/main/java/com/isp/backend/domain/schedule/repository/ScheduleRepository.java
+++ backend/src/main/java/com/isp/backend/domain/schedule/repository/ScheduleRepository.java
@@ -2,18 +2,20 @@ package com.isp.backend.domain.schedule.repository;
 
 import com.isp.backend.domain.member.entity.Member;
 import com.isp.backend.domain.schedule.entity.Schedule;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
-
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    Optional<Schedule> findById(Long id);
     Optional<Schedule> findByIdAndActivatedIsTrue(Long scheduleId);
+    @EntityGraph(attributePaths = {"checkLists"})
     @Query("SELECT s FROM Schedule s LEFT JOIN FETCH s.country WHERE s.member = :member AND s.activated = true ORDER BY s.updatedAt DESC")
     List<Schedule> findSchedulesByMember(@Param("member") Member member);
+    @EntityGraph(attributePaths = {"checkLists"})
     @Query("SELECT s FROM Schedule s LEFT JOIN FETCH s.scheduleDetails WHERE s.member = :member ORDER BY s.id DESC")
     List<Schedule> findTop5ByMemberOrderByIdDescWithDetails(@Param("member") Member member);
 

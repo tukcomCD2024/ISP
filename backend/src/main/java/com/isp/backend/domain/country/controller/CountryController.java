a16aae8dfdc20dd940b3d170e1ac941054fa9049
Dayeon-Hong <dayon0325@gmail.com>
2024-06-05 16:17:13 +0900

    feat : 현재 날씨, 이번주 날씨 조회 API 추가 및 날씨 설명 json 값

--- backend/src/main/java/com/isp/backend/domain/country/controller/CountryController.java
+++ backend/src/main/java/com/isp/backend/domain/country/controller/CountryController.java
@@ -1,7 +1,9 @@
 package com.isp.backend.domain.country.controller;
 
 import com.isp.backend.domain.country.dto.request.LocationRequest;
+import com.isp.backend.domain.country.dto.response.DailyWeatherResponse;
 import com.isp.backend.domain.country.dto.response.LocationResponse;
+import com.isp.backend.domain.country.dto.response.WeatherResponse;
 import com.isp.backend.domain.country.service.CountryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -30,6 +34,26 @@ public class CountryController {
             return ResponseEntity.notFound().build();
         }
     }
+
+
+    /** 현재 날씨 정보 가져오기 **/
+    @GetMapping("/weather/current")
+    public ResponseEntity<WeatherResponse> getCurrentWeather(@RequestBody LocationRequest requestDTO) {
+        String city = requestDTO.getCountry();
+        WeatherResponse weatherResponse = countryService.getCurrentWeather(city);
+        return ResponseEntity.ok(weatherResponse);
+    }
+
+
+    /** 한 주 날씨 정보 조회 **/
+    @GetMapping("/weather/weekly")
+    public ResponseEntity<List<DailyWeatherResponse>> getWeeklyWeather(@RequestBody LocationRequest requestDTO) {
+        String city = requestDTO.getCountry();
+        LocalTime requestedTime = LocalTime.of(12, 0);
+        List<DailyWeatherResponse> weeklyWeather = countryService.getWeeklyWeather(city, requestedTime);
+        return ResponseEntity.ok(weeklyWeather);
+    }
+
 
 
 }

4d286169f3a528f8aa1615637520395e5dfe347a
Dayeon-Hong <dayon0325@gmail.com>
2024-06-17 00:49:04 +0900

    fix : 항공권 좋아요 API 응답값 id 반환으로 수정

--- backend/src/main/java/com/isp/backend/domain/flight/service/FlightOfferService.java
+++ backend/src/main/java/com/isp/backend/domain/flight/service/FlightOfferService.java
@@ -1,12 +1,10 @@
 package com.isp.backend.domain.flight.service;
 
 import com.amadeus.exceptions.ResponseException;
-import com.isp.backend.domain.country.entity.Country;
 import com.isp.backend.domain.flight.dto.request.FlightLikeRequest;
 import com.isp.backend.domain.flight.dto.request.FlightSearchRequest;
 import com.isp.backend.domain.flight.dto.request.SkyScannerRequest;
 import com.isp.backend.domain.flight.dto.response.FlightLikeResponse;
-import com.isp.backend.global.exception.schedule.CountryNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,7 +16,7 @@ public interface FlightOfferService {
     String generateSkyscannerUrl(SkyScannerRequest request);
 
     @Transactional
-    void addLikeFlight(String uid, FlightLikeRequest flightLikeRequest);
+    Long addLikeFlight(String uid, FlightLikeRequest flightLikeRequest);
 
     List<FlightLikeResponse> getLikedFlights(String memberUid);
 

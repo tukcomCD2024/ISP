73cfd83084b17e8a5fb3d7d1a3f679f2aa34af27
Kim Jiyoon <rlawldbs0911@naver.com>
2024-06-16 23:23:35 +0900

    feat: WebClient 빈 커스텀을 위한 구성 파일 작성

---
+++ backend/src/main/java/com/isp/backend/domain/gpt/config/WebClientConfig.java
@@ -0,0 +1,26 @@
+package com.isp.backend.domain.gpt.config;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000)
+                .doOnConnected(conn -> conn
+                        .addHandlerLast(new ReadTimeoutHandler(10))
+                        .addHandlerLast(new WriteTimeoutHandler(10)));
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+}

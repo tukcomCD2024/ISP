76c088a573295ac8276f17b557488ff3673724ff
Dayeon-Hong <dayon0325@gmail.com>
2024-06-16 23:04:33 +0900

    hotfix : repository checklist 참조 오류 해결

--- backend/src/main/java/com/isp/backend/domain/schedule/repository/ScheduleRepository.java
+++ backend/src/main/java/com/isp/backend/domain/schedule/repository/ScheduleRepository.java
@@ -12,10 +12,10 @@ import java.util.Optional;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     Optional<Schedule> findById(Long id);
     Optional<Schedule> findByIdAndActivatedIsTrue(Long scheduleId);
-    @EntityGraph(attributePaths = {"checkLists"})
+
     @Query("SELECT s FROM Schedule s LEFT JOIN FETCH s.country WHERE s.member = :member AND s.activated = true ORDER BY s.updatedAt DESC")
     List<Schedule> findSchedulesByMember(@Param("member") Member member);
-    @EntityGraph(attributePaths = {"checkLists"})
+
     @Query("SELECT s FROM Schedule s LEFT JOIN FETCH s.scheduleDetails WHERE s.member = :member ORDER BY s.id DESC")
     List<Schedule> findTop5ByMemberOrderByIdDescWithDetails(@Param("member") Member member);
 

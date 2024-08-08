3a68c7b18086958c4589ee00be43196102a43c25
Kim Jiyoon <rlawldbs0911@naver.com>
2024-08-08 13:43:32 +0900

    delete: 불필요한 파일 삭제

--- backend/src/main/java/com/isp/backend/domain/image/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.isp.backend.domain.image.repository;
-
-import com.isp.backend.domain.image.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, String> {
-}

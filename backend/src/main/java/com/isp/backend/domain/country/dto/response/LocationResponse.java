bbc8444f8ec8bb7c8a47a2677062379928ad70e1
Dayeon-Hong <dayon0325@gmail.com>
2024-08-13 23:53:04 +0900

    fix : 나라 찾기 API 수정

--- backend/src/main/java/com/isp/backend/domain/country/dto/response/LocationResponse.java
+++ backend/src/main/java/com/isp/backend/domain/country/dto/response/LocationResponse.java
@@ -12,12 +12,16 @@ public class LocationResponse {
     private double latitude;
 
     private double longitude;
+    private String currencyName ;
 
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+    }
+    public void setCurrencyName(String currencyName) {
+        this.currencyName = currencyName;
     }
 
 }

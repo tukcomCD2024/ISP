824257eca6defa6acef0a83defe9d5f1fd4dfcc7
Kim Jiyoon <rlawldbs0911@naver.com>
2024-09-01 19:43:43 +0900

    [#143] Hotfix: 스케줄별 저장한 이미지를 반환할 때 scheduleImageId도 함께 반환하도록 수정

--- backend/src/main/java/com/isp/backend/domain/scheduleImage/dto/response/ReadScheduleImageResponse.java
+++ backend/src/main/java/com/isp/backend/domain/scheduleImage/dto/response/ReadScheduleImageResponse.java
@@ -3,11 +3,11 @@ package com.isp.backend.domain.scheduleImage.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.Map;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class ReadScheduleImageResponse {
     private Long scheduleId;
-    private Map<String, String> pathAndSaveDate;
+    private List<ScheduleImageResponse> scheduleImages;
 }

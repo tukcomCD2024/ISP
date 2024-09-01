5ecc8e33843df7414dc02dcb83437b0c80493165
Kim Jiyoon <rlawldbs0911@naver.com>
2024-09-01 19:44:37 +0900

    [#143] Hotfix: 스케줄 이미지 응답을 리스트로 묶기 위한 비즈니스 로직 추가

--- backend/src/main/java/com/isp/backend/domain/scheduleImage/service/ScheduleImageService.java
+++ backend/src/main/java/com/isp/backend/domain/scheduleImage/service/ScheduleImageService.java
@@ -5,10 +5,7 @@ import com.isp.backend.domain.schedule.dto.response.ScheduleListResponse;
 import com.isp.backend.domain.schedule.entity.Schedule;
 import com.isp.backend.domain.schedule.repository.ScheduleRepository;
 import com.isp.backend.domain.scheduleImage.dto.request.SaveScheduleImageRequest;
-import com.isp.backend.domain.scheduleImage.dto.response.ReadScheduleImageCountriesResponse;
-import com.isp.backend.domain.scheduleImage.dto.response.ReadScheduleImageResponse;
-import com.isp.backend.domain.scheduleImage.dto.response.SaveScheduleImageResponse;
-import com.isp.backend.domain.scheduleImage.dto.response.ScheduleLocationResponse;
+import com.isp.backend.domain.scheduleImage.dto.response.*;
 import com.isp.backend.domain.scheduleImage.entity.ScheduleImage;
 import com.isp.backend.domain.scheduleImage.repository.ScheduleImageRepository;
 import com.isp.backend.domain.scheduleImage.repository.ScheduleImageS3Repository;
@@ -65,13 +62,11 @@ public class ScheduleImageService implements SaveScheduleImageService, ReadSched
 	@Override
 	public ReadScheduleImageResponse read(Long scheduleId) {
 		List<ScheduleImage> scheduleImages = scheduleImageRepository.findByScheduleId(scheduleId);
-		Map<String, String> pathSaveDateMap = new HashMap<>();
+		List<ScheduleImageResponse> scheduleImageResponses = scheduleImages.stream()
+				.map(scheduleImage -> new ScheduleImageResponse(scheduleImage.getId(), scheduleImage.getPath(), scheduleImage.getSaveDate()))
+				.collect(Collectors.toList());
 
-		scheduleImages.forEach(scheduleImage ->
-				pathSaveDateMap.put(scheduleImage.getSaveDate(), scheduleImage.getPath())
-		);
-
-		return new ReadScheduleImageResponse(scheduleId, pathSaveDateMap);
+		return new ReadScheduleImageResponse(scheduleId, scheduleImageResponses);
 	}
 
 	@Override
